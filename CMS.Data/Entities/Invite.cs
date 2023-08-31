01d39b6ebb952a77681fe9af983fd301c8695eb9
Olatbossun <boyxdik@gmail.com>
2023-08-31 19:12:10 +0100

    Project Set-up and models

---
+++ CMS.Data/Entities/Invite.cs
@@ -0,0 +1,10 @@
+﻿namespace CMS.Data.Entities
+{
+    public class Invite : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ CMS.Data/Entities/Stack.cs
@@ -0,0 +1,8 @@
+﻿namespace CMS.Data.Entities
+{
+    public class Stack : BaseEntity
+    {
+        public string? StackName { get; set; }
+        public ICollection<UserStack> User { get; set; }
+    }
+}
